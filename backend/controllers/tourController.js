a6b6721500b46943817e0089cf6a9368242b5634
kunal <mskunal07@gmail.com>
2023-05-24 20:58:56 +0530

    booking part done 3rd complete

--- backend/controllers/tourController.js
+++ backend/controllers/tourController.js
@@ -82,7 +82,7 @@ export const getSingleTour = async(req,res)=>{
 
     try{
 
-        const tour = await Tour.findById(id);
+        const tour = await Tour.findById(id).populate('reviews');
 
         res.status(200).json({
             success:true,
@@ -107,7 +107,7 @@ export const getAllTour = async(req,res)=>{
 
     try{
 
-        const tours = await Tour.find({}).skip(page * 8).limit(8);
+        const tours = await Tour.find({}).populate('reviews').skip(page * 8).limit(8);
 
         res.status(200).json({
             success:true,
@@ -138,7 +138,7 @@ export const getTourBySearch  = async(req,res) =>{
 
         // gte means greater than equal
         const tours = await Tour.find({city,distance :{$gte:distance},
-        maxGroupSize:{$gte:maxGroupSize} })
+        maxGroupSize:{$gte:maxGroupSize} }).populate('reviews');
 
         res.status(200).json({
             success:true,
@@ -160,7 +160,7 @@ export const getFeaturedTour = async(req,res)=>{
 
     try{
 
-        const tours = await Tour.find({featured:true}).limit(8);
+        const tours = await Tour.find({featured:true}).populate('reviews').limit(8);
 
         res.status(200).json({
             success:true,
